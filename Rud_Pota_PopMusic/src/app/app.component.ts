c2ec2bb6852e58e97dcc9caf95e531411f1ec10b
Rudri Pota <W0817379@myscc.ca>
2024-01-28 18:38:24 -0600

    added everything else

--- Rud_Pota_PopMusic/src/app/app.component.ts
+++ Rud_Pota_PopMusic/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContentCardComponent } from "./content-card/content-card.component";
+import { ContentListComponent } from "./content-list/content-list.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, ContentCardComponent]
+    imports: [CommonModule, RouterOutlet, ContentCardComponent, ContentListComponent]
 })
 export class AppComponent {
   title = 'Pop Music by Rudri';
